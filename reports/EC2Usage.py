b2d432d0364568be65a73711a2b3c2b2f064bc66
Leandro Souza <leandro.souza@hotelurbano.com.br>
2016-01-29 16:37:28 -0200

    check reserved instances

--- reports/EC2Usage.py
+++ reports/EC2Usage.py
@@ -1,32 +1,70 @@
 #!/usr/bin/python
 
-import boto.ec2
-from collections import defaultdict, Counter
-
-report = defaultdict(Counter)
-
-def get_ec2_instances(region):
-    ec2_conn = boto.ec2.connect_to_region(region)
-    res = ec2_conn.get_all_instances()
-    instances = [i for r in res for i in r.instances]
-    vol = ec2_conn.get_all_volumes()
-    for volumes in vol:
-    	if volumes.attachment_state() == 'attached':
-   			filter = {'block-device-mapping.volume-id':volumes.id}
-   			volumesinstance = ec2_conn.get_all_instances(filters=filter)
-			ids = [z for k in volumesinstance for z in k.instances]
-			for s in ids:
-				report[s.placement][s.instance_type] += 1
-				#print 'Zone:' + s.placement,' - Instance ID:' + s.id + ' - Instance Type:' + s.instance_type + ' - Attached Volume ID:' + volumes.id +' - Device Name:' + volumes.attach_data.device
+import sys
+import os
+import boto
+from pprint import pprint
 
 
-def main():
-    regions = ['us-east-1','us-west-1','us-west-2','eu-west-1','sa-east-1',
-                'ap-southeast-1','ap-southeast-2','ap-northeast-1']
-    
-    for region in regions:
-    	get_ec2_instances(region)
+ec2_conn = boto.connect_ec2()
+reservations = ec2_conn.get_all_instances()
 
-    print report
+running_instances = {}
+for reservation in reservations:
+	for instance in reservation.instances:
+		if instance.state != "running":
+			sys.stderr.write("Disqualifying instance %s: not running\n" % ( instance.id ) )
+		elif instance.spot_instance_request_id:
+			sys.stderr.write("Disqualifying instance %s: spot\n" % ( instance.id ) )
+		else:
+			az = instance.placement
+			instance_type = instance.instance_type
+			running_instances[ (instance_type, az ) ] = running_instances.get( (instance_type, az ) , 0 ) + 1
 
-if  __name__ =='__main__':main()
+
+# pprint( running_instances )
+
+
+reserved_instances = {}
+for reserved_instance in ec2_conn.get_all_reserved_instances():
+	if reserved_instance.state != "active":
+		sys.stderr.write( "Excluding reserved instances %s: no longer active\n" % ( reserved_instance.id ) )
+	else:
+		az = reserved_instance.availability_zone
+		instance_type = reserved_instance.instance_type
+		reserved_instances[( instance_type, az) ] = reserved_instances.get ( (instance_type, az ), 0 )  + reserved_instance.instance_count
+
+# pprint( reserved_instances )
+
+
+# this dict will have a positive number if there are unused reservations
+# and negative number if an instance is on demand
+instance_diff = dict([(x, reserved_instances[x] - running_instances.get(x, 0 )) for x in reserved_instances])
+
+# instance_diff only has the keys that were present in reserved_instances. There's probably a cooler way to add a filtered dict here
+for placement_key in running_instances:
+	if not placement_key in reserved_instances:
+		instance_diff[placement_key] = -running_instances[placement_key]
+
+# pprint ( instance_diff )
+
+unused_reservations = dict((key,value) for key, value in instance_diff.iteritems() if value > 0)
+if unused_reservations == {}:
+	print "Congratulations, you have no unused reservations"
+else:
+	for unused_reservation in unused_reservations:
+		print "UNUSED RESERVATION!\t(%s)\t%s\t%s" % ( unused_reservations[ unused_reservation ], unused_reservation[0], unused_reservation[1] )
+
+print ""
+
+unreserved_instances = dict((key,-value) for key, value in instance_diff.iteritems() if value < 0)
+if unreserved_instances == {}:
+	print "Congratulations, you have no unreserved instances"
+else:
+	for unreserved_instance in unreserved_instances:
+		print "Instance not reserved:\t(%s)\t%s\t%s" % ( unreserved_instances[ unreserved_instance ], unreserved_instance[0], unreserved_instance[1] )
+
+qty_running_instances = reduce( lambda x, y: x+y, running_instances.values() )
+qty_reserved_instances = reduce( lambda x, y: x+y, reserved_instances.values() )
+
+print "\n(%s) running on-demand instances\n(%s) reservations" % ( qty_running_instances, qty_reserved_instances )
