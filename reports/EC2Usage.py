98a4b91df99dd353e393a1247f6cff8e327360a0
Leandro Souza <leandro.souza@hotelurbano.com.br>
2016-01-29 12:29:34 -0200

    RegionInfo - Instance ID - Instance Type - Attached Volume ID - Device Name

--- reports/EC2Usage.py
+++ reports/EC2Usage.py
@@ -4,9 +4,18 @@ import boto.ec2
 
 def get_ec2_instances(region):
     ec2_conn = boto.ec2.connect_to_region(region)
-    instances = ec2_conn.get_all_instances()
-    for inst in instances:
-        print region+':',inst.instances
+    res = ec2_conn.get_all_instances()
+    instances = [i for r in res for i in r.instances]
+    vol = ec2_conn.get_all_volumes()
+
+    for volumes in vol:
+    	if volumes.attachment_state() == 'attached':
+   			filter = {'block-device-mapping.volume-id':volumes.id}
+   			volumesinstance = ec2_conn.get_all_instances(filters=filter)
+			ids = [z for k in volumesinstance for z in k.instances]
+			for s in ids:
+				print s.region,'- Instance ID:' + s.id + ' - Instance Type:' + s.instance_type + ' - Attached Volume ID:' + volumes.id +' - Device Name:' + volumes.attach_data.device
+
 
 def main():
     regions = ['us-east-1','us-west-1','us-west-2','eu-west-1','sa-east-1',
